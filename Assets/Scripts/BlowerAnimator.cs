218f437a265bcf438282af30c459608c967630be
leonardstalder <leonardstalder@gmail.com>
2013-11-18 11:23:08 +0100

    Blower Animator

---
+++ Assets/Scripts/BlowerAnimator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class BlowerAnimator : MonoBehaviour {
+	
+	
+	// Update is called once per frame.
+	void Update ()
+	{
+		rotatePropellers(); 	// Rotate the propellers.
+	}
+	
+	// --------------------------------------------------------------
+	// Ship animations.
+	// --------------------------------------------------------------
+	
+	// Rotate the propellers.
+	void rotatePropellers()
+	{
+		GameObject.Find("Propeller").transform.Rotate(Vector3.forward, Time.deltaTime * 4);
+	}
+}
+
