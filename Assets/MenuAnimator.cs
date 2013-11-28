f88722c58d2a079506cdc5a7a518d9143add0d96
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2013-11-28 20:49:26 +0100

    Game menu created + zfight bug correction

---
+++ Assets/MenuAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAnimator : MonoBehaviour {
+
+	// Required Gameobjects.
+	public GameObject propellerLeft;
+	public GameObject propellerRight;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Rotate propellers
+		propellerLeft.transform.Rotate(Vector3.up, Time.deltaTime * 100000);
+		propellerRight.transform.Rotate(Vector3.up, Time.deltaTime * 100000);
+	}
+}
