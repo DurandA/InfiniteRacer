fb9800a152d7833502d363b44c15cc4ef30701f5
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2013-12-01 16:22:28 +0100

    Bug resolution on Menu

--- Assets/Scripts/Scene/Loader.cs
+++ Assets/Scripts/Scene/Loader.cs
@@ -26,7 +26,7 @@ public class Loader : MonoBehaviour {
 		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height), loadingTexture, ScaleMode.StretchToFill);
 
 		GUI.skin = skin;
-		if(GUI.Button (new Rect ((Screen.width/2) - ((Screen.width * 0.25f)/2) , (Screen.height * 0.85hf),(Screen.width * 0.25f),(Screen.height * 0.1f)), "START"))
+		if(GUI.Button (new Rect ((Screen.width/2) - ((Screen.width * 0.25f)/2) , (Screen.height * 0.85f),(Screen.width * 0.25f),(Screen.height * 0.1f)), "START"))
 		{
 			Time.timeScale = 1;
 			Destroy (gameObject);
