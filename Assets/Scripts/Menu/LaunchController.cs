d324154cc0d8f7627772102d6ceec39e0bee52f5
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2013-12-05 23:53:11 +0100

    Animations in main menu
    bitches love it

--- Assets/Scripts/Menu/LaunchController.cs
+++ Assets/Scripts/Menu/LaunchController.cs
@@ -4,7 +4,12 @@ using System.Collections;
 // Simply resumes the time when coming from the pause menu back
 // to the main menu.
 public class LaunchController : MonoBehaviour {
+	public GameObject cameraAnimation;
+
+	// Initialization.
 	void Start () {
 		Time.timeScale = 1;
+
+
 	}
 }
