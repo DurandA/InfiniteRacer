84a2f6643c01ea77bbae34e72bd479ff53e9cc56
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2013-12-10 23:51:42 +0100

    Menu is done bitches !

---
+++ Assets/AnimSoundTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimSoundTrigger : MonoBehaviour {
+
+	public AudioSource flyby1;
+	public AudioSource flyby2;
+
+	void triggerFlyBy1()
+	{
+		flyby1.audio.Play();
+	}
+
+	void triggerFlyBy2()
+	{
+		flyby2.audio.Play();
+	}
+}
