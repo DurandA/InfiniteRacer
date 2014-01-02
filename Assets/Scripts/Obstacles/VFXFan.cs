6a2f5afb7bce2ad080bcd976c81fbb65fe43268d
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2014-01-02 23:30:18 +0100

    Graphical improvements

---
+++ Assets/Scripts/Obstacles/VFXFan.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Author : Thomas Rouvinez
+ */
+
+public class VFXFan : MonoBehaviour {
+
+	private int random;
+
+	void Start()
+	{
+		random = Random.Range (100, 150);
+	}
+
+	void Update () {
+		transform.Rotate(Vector3.up, Time.deltaTime * random);
+	}
+}
