30e03ff8c25cfed710b01e7e3880c45777af74eb
Arnaud Durand <arnaud.durand@unifr.ch>
2014-01-21 23:49:13 +0100

    Badname fix #2

---
+++ Assets/Scripts/Navigation/Pipe90HalfSBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe90HalfSBehaviour : PipeBehaviour {
+
+	public void Awake(){
+
+		createBlower(0.1f);
+		createLaser(0.3f);	
+		SpawnCoins(0.3f,7,8.0f);
+
+
+	}
+
+}
